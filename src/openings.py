cae71969996f6be276c10b77afcd6693af694b27
Aidan Carroll <a22carroll@gmail.com>
2025-07-24 18:00:06 +0000

    added more openings

--- src/openings.py
+++ src/openings.py
@@ -3,5 +3,15 @@ openings = {
     "Ruy Lopez": ["e4", "e5", "Nf3", "Nc6", "Bb5"],
     "Italian Game": ["e4", "e5", "Nf3", "Nc6", "Bc4", "Bc5"],
     "Sicilian Defense": ["e4", "c5", "Nf3", "d6", "d4", "cxd4"],
+    "Sicilian Najdorf": ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "a6"],
+    "Sicilian Dragon": ["e4", "c5", "Nf3", "d6", "d4", "cxd4", "Nxd4", "Nf6", "Nc3", "g6"],
+    "French Defense": ["e4", "e6", "d4", "d5"],
+    "Caro-Kann Defense": ["e4", "c6", "d4", "d5"],
+    "Queen's Gambit": ["d4", "d5", "c4"],
+    "Queen's Gambit Declined": ["d4", "d5", "c4", "e6"],
+    "King's Indian Defense": ["d4", "Nf6", "c4", "g6", "Nc3", "Bg7", "e4", "d6"],
+    "English Opening": ["c4", "e5", "Nc3", "Nf6"],
+    "Scandinavian Defense": ["e4", "d5", "exd5", "Qxd5", "Nc3", "Qa5"],
+   
 
 }
